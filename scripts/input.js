ae3b5f764ad3b34793b615ac30423c8619f32297
Chris Adams <chris.ah.adams@gmail.com>
2016-02-01 19:19:41 -0400

    completed keycode lookup table
    adapted code from:
    https://github.com/timoxley/keycode/blob/master/index.js

--- scripts/input.js
+++ scripts/input.js
@@ -1,12 +1,118 @@
-BSWG.KEY = {
-    LEFT:  37,
-    RIGHT: 39,
-    UP:    38,
-    DOWN:  40,
+BSWG.KEY = (function(){
 
-    SPACE: 32,
-    ESC:   27
-};
+    // From: https://github.com/timoxley/keycode/blob/master/index.js
+
+    var codes = {
+      'backspace': 8,
+      'tab': 9,
+      'enter': 13,
+      'shift': 16,
+      'ctrl': 17,
+      'alt': 18,
+      'pause/break': 19,
+      'caps lock': 20,
+      'esc': 27,
+      'space': 32,
+      'page up': 33,
+      'page down': 34,
+      'end': 35,
+      'home': 36,
+      'left': 37,
+      'up': 38,
+      'right': 39,
+      'down': 40,
+      'insert': 45,
+      'delete': 46,
+      'command': 91,
+      'right click': 93,
+      'numpad *': 106,
+      'numpad +': 107,
+      'numpad -': 109,
+      'numpad .': 110,
+      'numpad /': 111,
+      'num lock': 144,
+      'scroll lock': 145,
+      'my computer': 182,
+      'my calculator': 183,
+      ';': 186,
+      '=': 187,
+      ',': 188,
+      '-': 189,
+      '.': 190,
+      '/': 191,
+      '`': 192,
+      '[': 219,
+      '\\': 220,
+      ']': 221,
+      "'": 222,
+    }
+
+    // Helper aliases
+
+    var aliases = {
+      'windows': 91,
+      '⇧': 16,
+      '⌥': 18,
+      '⌃': 17,
+      '⌘': 91,
+      'ctl': 17,
+      'control': 17,
+      'option': 18,
+      'pause': 19,
+      'break': 19,
+      'caps': 20,
+      'return': 13,
+      'escape': 27,
+      'spc': 32,
+      'pgup': 33,
+      'pgdn': 33,
+      'ins': 45,
+      'del': 46,
+      'cmd': 91
+    }
+
+
+    /*!
+     * Programatically add the following
+     */
+
+    // lower case chars
+    for (i = 97; i < 123; i++) codes[String.fromCharCode(i)] = i - 32
+
+    // numbers
+    for (var i = 48; i < 58; i++) codes[i - 48] = i
+
+    // function keys
+    for (i = 1; i < 13; i++) codes['f'+i] = i + 111
+
+    // numpad keys
+    for (i = 0; i < 10; i++) codes['numpad '+i] = i + 96
+
+    /**
+     * Get by code
+     *
+     *   exports.name[13] // => 'Enter'
+     */
+
+    var names = {} // title for backward compat
+
+    // Create reverse mapping
+    for (i in codes) names[codes[i]] = i;
+
+    // Add aliases
+    for (var alias in aliases) {
+      codes[alias] = aliases[alias]
+    }
+
+    BSWG.KEY_NAMES = names;
+
+    var ret = {};
+    for (var key in codes)
+        ret[key.toUpperCase()] = codes[key];
+
+    return ret;
+
+})();
 
 BSWG.input = new function(){
 
