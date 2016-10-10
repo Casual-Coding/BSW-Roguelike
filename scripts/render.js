9f0882b3cfa48ffa1989db48d3329c1ea0a4bc6a
Chris Adams <chris.ah.adams@gmail.com>
2016-10-10 16:19:15 -0300

    deeper fps smoothing

--- scripts/render.js
+++ scripts/render.js
@@ -592,7 +592,7 @@ BSWG.render = new function() {
     this.cursorNo = 0;
     this.cursorScale = 1.0;
 
-    var last60dt = new Array(6);
+    var last60dt = new Array(20);
     for (var i=0; i<last60dt.length; i++) {
         last60dt[i] = 1.0/60;
     }
